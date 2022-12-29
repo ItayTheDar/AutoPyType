7c050779ea303ee066061c992c344d97fe50a298
itayd <itay.dar@beyondminds.ai>
2022-12-29 17:32:54 +0200

    re arrange file structure

--- main.py
+++ main.py
@@ -16,104 +16,102 @@ then you can run the output file and see the results.
 """
 import os
 from pathlib import Path
-from typing import List, Union, Dict, Any, Optional, Tuple, Callable, TypeVar, Generic, Type, Set, Iterable, Iterator
 import timeit
 import openai
 import logging
+from openai.openai_object import OpenAIObject
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def get_file_from_path(path: Path) -> str:
+def send_request_to_openai(model: str, prompt: str, max_tokens: int) -> OpenAIObject:
     """
-    get file from path
-    :param path: path to the python file that user want to add type hints
-    :return: string of the python file
+    send request to openai by using the completion api
+    :param model: model name (e.g. "text-davinci-003")
+    :param prompt: the prompt to send to openai
+    :param max_tokens: number of tokens to send to openai
+    :return: the response from openai
     """
-    with open(path, 'r') as f:
-        return f.read()
+    return openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        max_tokens=max_tokens
+    )
 
 
-def send_file_in_batch_to_gpt(file: str, batch_size: int = 10) -> List[str]:
+def create_typed_python_file(api_key: str, prompt: str, window: int = 20, max_len: int = 100, max_tokens=42) -> str:
     """
-    send file in batch to gpt
-    :param file: the file to send
-    :param batch_size: the batch size
-    :return: list of batches
+    create typed python file by using openai api, and especially the chatgpt model that help us to add type hints, logs and comments, and more.
+    :param api_key: the api key of openai - you can get it from https://beta.openai.com/
+    :param prompt: the prompt to send to openai (the python file + instructions what to do)
+    :param window: the window size - which part of the prompt to send to openai (the last window size words
+    :param max_len: the maximum length of the output
+    :param max_tokens: the maximum number of tokens to send to openai in each batch
+    :return: the function return a python file in a string format.
     """
-    return [file[i:i + batch_size] for i in range(0, len(file), batch_size)]
-
-
-def create_connection_openai():
-    """create connection openai"""
-    pass
-
-
-def send_batch_to_openai(batch: str) -> str:
-    """send batch to openai"""
-    pass
-
-
-def send_file_to_openai(file: str, batch_size: int = 10) -> List[str]:
-    """send file to openai"""
-    batches = send_file_in_batch_to_gpt(file, batch_size)
-    return [send_batch_to_openai(batch) for batch in batches]
-
-
-def create_typed_python_file(api_key: str, prompt: str, window: int = 20, max_len: int = 100, max_tokens=42):
     global location_end_prompt
-    import openai
-    logger.info("creating a story")
     openai.api_key = api_key
     logger.info("api key is set")
-    story = prompt
-    logger.info("story is set")
+    code = prompt
     i = 0
-    while len(story.split(" ")) < max_len:
-        logger.info("story is less than max_len - starting to generate the next piece of the story")
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=max_tokens
-        )
-        logger.info(f"response is ready - {response.choices[0].text}")
-        old_story = story
+    while len(code.split(" ")) < max_len:
+        logger.info("code is less than max_len - starting to generate the next piece of the code")
+        response = send_request_to_openai("text-davinci-003", code, max_tokens)
+        logger.info(f"response is ready")
+        old_code = code
         if i == 0:
-            location_end_prompt = len(old_story)
-        story += response.choices[0].text
-        if old_story == story:
-            logger.info("story is not changing - breaking the loop")
+            location_end_prompt = len(old_code)
+        code += response.choices[0].text
+        if old_code == code:
+            logger.info("code is not changing - breaking the loop")
             break
-        logger.info(f"story is ready - {story}")
-        prompt = " ".join(story.split(" ")[-window:])
+        logger.info(f"code is ready - {code}")
+        prompt = " ".join(code.split(" ")[-window:])
         logger.info(f"next prompt is ready - {prompt}")
-    import pprint
-    pprint.pprint(story)
+    # import pprint
+    # pprint.pprint(code)
 
-    return story[location_end_prompt:]
+    return code[location_end_prompt:]
 
 
 if __name__ == '__main__':
-    # prompt = "please complete this story: \nThis is story about two friends that went out for a tour in northern india, they've went down from the bus in the city of delhi and then ...  "
-    # prompt = "please generate a function that will get a list of numbers and return the sum of the numbers: \n def sum_of_numbers(numbers):"
-    # prompt = "please create a small flask application that helps students to sign they're courses\n Notes: make this function with types, logs and well documented:"
-    python_file = open("/Users/itayd/PycharmProjects/AutoPyType/examples/example.py", "r").read()
-    prompt = "please add type hints to each and every variable in each" \
-             " class or function. if the type is not yet imported, " \
-             "please import it. also, please add logs to the code using logging library." \
-             "\n this is the code:\n" + python_file
+    ABSOLUTE_PATH = Path(__file__).parent.absolute()
+    python_path = ABSOLUTE_PATH / "examples/example.py"
+    python_file = open(python_path, "r").read()
+    base_prompt = "\n this is the code:\n" + python_file
     base_tokens = 4096
-    python_typed_file = create_typed_python_file(
-        api_key=os.getenv('OPENAI_API_KEY'),
-        prompt=prompt,
-        window=50,
-        max_len=len(prompt.split(" ")) + 100,
-        max_tokens=base_tokens - len(prompt.split(" "))
-    )
-    print(python_typed_file)
-    with open("/Users/itayd/PycharmProjects/AutoPyType/examples/example_typed.py", "w") as f:
-        f.write(python_typed_file)
+    instructions = [
+        "please add type hints to each and every variable in each class or function, remain imports as they are and add import statements if needed",
+        "add logs to the code using logging library",
+        "add documentation to each and every class or function",
+        # "please make sure that every package that is used in the code is imported and fix bugs if needed",
+    ]
+    for instruction in instructions:
+        s = timeit.default_timer()
+        logger.info(f"starting to generate code for instruction - {instruction}")
+        code = create_typed_python_file(
+            api_key=os.getenv("OPENAI_API_KEY"),
+            prompt= instruction + "\nthis is the code:\n" + base_prompt,
+            window=len(base_prompt.split(" ")) - 1,
+            max_len=len(base_prompt.split(" ")) + 100,
+            max_tokens=base_tokens - len(base_prompt.split(" ")) - len(instruction.split(" ")) - 5
+        )
+        base_prompt = code
+
+    with open(ABSOLUTE_PATH / "examples/example_typed_2.py", "w") as f:
+        f.write(base_prompt)
+
+
+
+
+    # python_typed_file = create_typed_python_file(
+    #     api_key=os.getenv('OPENAI_API_KEY'),
+    #     prompt=prompt,
+    #     window=20,
+    #     max_len=len(prompt.split(" ")) + 100,
+    #     max_tokens=base_tokens - len(prompt.split(" "))
+    # )
     # rand = "Today is the the day that I will start to learn how to use openai, this is so exciting, I can't wait to start. "
     #            "by gaining the abbility to use openai I will be able to do so many things, I will be able to create a "
 #         api_key = os.environ['OPENAI_API_KEY']
